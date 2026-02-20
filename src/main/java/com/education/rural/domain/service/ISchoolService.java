70e5604ca1286de6d391e784adf9f443f015c18f
Ronald Castro_pavilion <ronaldivan.1210868@correo.uis.edu.co>
2026-02-20 13:52:20 -0500

    Create a domain SchoolService And ISchoolService for manage dtos

---
+++ src/main/java/com/education/rural/domain/service/ISchoolService.java
@@ -0,0 +1,12 @@
+package com.education.rural.domain.service;
+
+import com.education.rural.domain.dto.SchoolDto;
+
+import java.util.List;
+
+public interface ISchoolService {
+
+    List<SchoolDto> findAllSchool();
+    SchoolDto getSchoolById(long schoolId);
+    SchoolDto addSchool(SchoolDto schoolDto);
+}
