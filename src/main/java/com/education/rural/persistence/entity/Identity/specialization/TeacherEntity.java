997247f582c180d2af871641941a25e5a2d11bdb
Ronald Castro_pavilion <ronaldivan.1210868@correo.uis.edu.co>
2026-03-30 15:31:25 -0500

    TeacherProfileMapper UpdateFromDto fixed

---
+++ src/main/java/com/education/rural/persistence/entity/Identity/specialization/TeacherEntity.java
@@ -0,0 +1,35 @@
+package com.education.rural.persistence.entity.Identity.specialization;
+
+
+import com.education.rural.persistence.entity.Identity.PersonEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "teacher")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TeacherEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Tea_Id")
+    private Long teacherId;
+
+    @Column(name = "Tea_Active")
+    private Boolean active=true;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="Tea_TpfId")
+    private TeacherProfileEntity teacherProfileEntity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "Tea_PerId")
+    private PersonEntity personEntity;
+
+}
--- src/main/java/com/education/rural/persistence/mapper/Identity/specialization/TeacherProfileMapper.java
+++ src/main/java/com/education/rural/persistence/mapper/Identity/specialization/TeacherProfileMapper.java
@@ -4,10 +4,7 @@ package com.education.rural.persistence.mapper.Identity.specialization;
 import com.education.rural.domain.dto.Identity.specialization.teacher.profile.TeacherProfileDto;
 import com.education.rural.domain.dto.Identity.specialization.teacher.profile.UpdateTeacherProfileDto;
 import com.education.rural.persistence.entity.Identity.specialization.TeacherProfileEntity;
-import org.mapstruct.BeanMapping;
-import org.mapstruct.InheritInverseConfiguration;
-import org.mapstruct.Mapper;
-import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.*;
 
 import java.util.List;
 
@@ -25,5 +22,5 @@ public interface TeacherProfileMapper {
 
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-    void updateTeacherToDto(UpdateTeacherProfileDto dto,TeacherProfileEntity entity);
+    void updateTeacherToDto(UpdateTeacherProfileDto dto,@MappingTarget TeacherProfileEntity entity);
 }
